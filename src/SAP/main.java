fb2d9b1c2f15809c4553ca6b19e12f02ee683bef
Dermn8er <33468194+Dermn8er@users.noreply.github.com>
2018-05-04 22:55:03 -0600

    Update main.java

--- src/SAP/main.java
+++ src/SAP/main.java
@@ -3,7 +3,9 @@ package SAP;
 public class main {
 
 	public static void main(String[] args) {
+		//Test Cases are F338258016, F333124653, F438688089, and F931238613
 		advisor test = new advisor("F338258016");
+		test.setMajor("CS");
 		test.start();
 		
 	}
